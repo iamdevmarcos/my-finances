0adf5f8b22eed0a63f2a59d44d02849b75eb4b78
Marcos Andre <marcosdev.me@gmail.com>
2022-02-05 14:21:40 -0300

    perf: Add Types

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,4 +1,4 @@
-import { Category } from "../types/category";
+import { Category } from "../types/Category";
 
 export const categories: Category = {
   food: { title: "Alimentação", color: "blue", expense: true },
