e161dd46072ee783c7c3807a9ce7ffac1bf659e3
Marcos Andre <marcosdev.me@gmail.com>
2022-02-05 13:42:56 -0300

    chore: First commit

---
+++ src/App.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as C from "./App.styles";
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Title>Finanças Pessoais</C.Title>
+    </C.Container>
+  );
+};
+
+export default App;
