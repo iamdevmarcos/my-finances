91e20fd996cc3c689d0e1de74d72787ac527a82a
Marcos Andre <marcosdev.me@gmail.com>
2022-02-05 14:00:17 -0300

    feat: Add first styles

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,7 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
+export const Header = styled.div`
+  background: darkblue;
+  height: 150px;
+  text-align: center;
+`;
+
 export const Title = styled.h1`
   margin: 0;
   padding: 0;
+  color: #fff;
+  padding-top: 30px;
+`;
+
+export const Body = styled.div`
+  margin: 0 auto;
+  max-width: 980px;
+  margin-bottom: 50px;
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import * as C from "./App.styles";
 const App = () => {
   return (
     <C.Container>
-      <C.Title>Finanças Pessoais</C.Title>
+      <C.Header>
+        <C.Title>Finanças Pessoais</C.Title>
+      </C.Header>
+      <C.Body>
+        {/* Area de informações */}
+
+        {/* Area de inserção */}
+
+        {/* Tabela de Itens */}
+      </C.Body>
     </C.Container>
   );
 };
