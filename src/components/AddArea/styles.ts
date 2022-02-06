950f1c6979c664e8b36cbece733f0ac9a8a80f05
Marcos Andre <marcosdev.me@gmail.com>
2022-02-05 21:51:33 -0300

    feat: Add InputArea

--- src/components/AddArea/styles.ts
+++ src/components/AddArea/styles.ts
@@ -10,3 +10,45 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
 `;
+
+export const InputLabel = styled.label`
+  flex: 1;
+  margin: 10px;
+`;
+
+export const InputTitle = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 30px;
+  padding: 0 5px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  border-radius: 5px;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  height: 30px;
+  padding: 0 5px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  border-radius: 5px;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 30px;
+  padding: 0 5px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  border-radius: 5px;
+  background-color: lightblue;
+  color: black;
+  cursor: pointer;
+
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
+`;
